01d2e4d42e88ec6a9899d7511b22fb3516c8e0b2
mohfaisalsapt07 <112527200+mohfaisalsapt07@users.noreply.github.com>
2022-09-05 08:34:25 +0700

    Add files via upload

---
+++ Python5-Tkj4.py
@@ -0,0 +1,7 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================================= RESTART: C:/Users/Asus/OneDrive/Dokumen/Python5-Tkj4.py/Python5-Tkj4.py =============================================
+ dollar jika di rupiahkan sebesar 29.79
+uero jika di konversikan ke yen sebesar 1531.97
+won jika di konversikan ke rupiah sebesr 440
---
+++ pythone 10 tkj 4.py
@@ -0,0 +1,53 @@
+# Rumus Persegi sisi * sisi
+
+
+sisi1=20
+sisi2=20
+luas_p = sisi1 * sisi2
+print("Luas persegi adalah", luas_p)
+
+
+#Rumus persegi panjang * Lebar
+
+Panjang = 10
+lebar = 20
+luas_pp = panjang * lebar
+
+print("Luas persegi adalah",luas_pp)
+
+#Rumus Trapesium 1/2 * (a+t) * tinggi
+print("\nTrapesium")
+a = 6
+b = 10
+t = 8
+luas_t = (a+b) * t / 2
+print("luas Trapesium adalah", luas_t)
+
+
+#Rumus Segitiga 1/2 * a * t
+
+
+a = 10
+b = 5
+luas_s = a * t / 2
+print("luas segitiga adalah", luas_s)
+
+
+#rumus jajar genjang  a * t
+
+
+a = 20
+t = 15
+luas_jg = a * t
+print ("luas jajar genjang adalah",luas_jg)
+
+
+# rumus lingkaran π × r × r . L
+
+
+phi = 3.14
+r = 10
+r = 20
+l = 15
+luas_= phi * r * r * l
+print ("luas lingkaran adalah", luas_l)
