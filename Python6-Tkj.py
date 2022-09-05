7aa84d8f54e22ffed79b85d60518aa608c6dd079
mohfaisalsapt07 <112527200+mohfaisalsapt07@users.noreply.github.com>
2022-09-05 09:53:00 +0700

    Add files via upload

---
+++ Python6-Tkj.py
@@ -0,0 +1,8 @@
+list_kosong = []
+list_buah = ['Pisang', 'Nanas', 'Melon', 'Durian']
+list_nilai = [80, 70, 90, 60]
+list_jawaban = [150, 33.33, 'Presiden Sukarno', False]
+print('list_kosong:', list_kosong)
+print('list_buah:', list_buah)
+print('list_nilai:', list_nilai)
+print('list_jawaban:', list_jawaban)
