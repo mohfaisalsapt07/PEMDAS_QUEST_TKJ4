c85321cce990348367ca382630641668cd1c8051
mohfaisalsapt07 <112527200+mohfaisalsapt07@users.noreply.github.com>
2022-10-10 10:34:23 +0700

    Add files via upload

---
+++ pythone10-tkj4.py
@@ -0,0 +1,25 @@
+# membuat tipe data koleksi
+
+koleksi_data_str = ["pisang","mangga","jambu","semangka"]
+
+koleksi_data_int = [200,300,400,500]
+
+koleksi_data_mix = ["rizky billar",100,True,"reza arab"]
+
+print("koleksi data string:",koleksi_data_str)
+
+print("koleksi data integer:",koleksi_data_int)
+
+print("koleksi data campuran:",koleksi_data_mix)
+
+koleksi_nama_hewan = ["gajah","harimau","singa"]
+
+koleksi_nilai_uts = [77,89,90]
+
+koleksi_teman_sebangku= ["pai","faisal","santo"]
+
+print("koleksi nama hewan :",koleksi_nama_hewan)
+
+print("koleksi nilai uts:",koleksi_nilai_uts)
+
+print("koleksi teman sebangku:",koleksi_teman_sebangku)
